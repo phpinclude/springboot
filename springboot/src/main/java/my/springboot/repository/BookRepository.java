84451e58b86fe6ab9e45c10b48cd64f58bfb7da0
psm73 <psm73@psm73-PC>
2019-05-07 09:41:54 +0900

    Revert "최초배포"
    This reverts commit d068a3561c97ee7c03b016a044394136b77348f2.

--- springboot/src/main/java/my/springboot/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package my.springboot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import my.springboot.domain.Book;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-}
